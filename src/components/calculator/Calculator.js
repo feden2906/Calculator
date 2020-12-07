6cbf2cbccdf850ba73451b7ea123a5fad20ab24d
Fedenko Maksym <feden2906@gmail.com>
2020-12-07 15:56:10 +0200

    calculator update

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -8,16 +8,12 @@ import InputWrapper from "./inputWrapper/InputWrapper";
 
 class Calculator extends Component {
 
-  state = {result: 0}
+  state = {result: 0};
+  arrNums = [1, -1, 100, -100];
 
-  plus = (num) => {
+  plus_minus = (num) => {
     const {result} = this.state
     const value = result + num
-    this.setState({result: value})
-  }
-  minus = (num) => {
-    const {result} = this.state
-    const value = result - num
     value > 0
         ? this.setState({result: value})
         : this.setState({result: 0})
@@ -26,6 +22,7 @@ class Calculator extends Component {
   addYourNum = (e) => {
     const {result} = this.state
     let userNum = parseFloat(e.target.previousSibling.value)
+    e.target.previousSibling.value = ''
     if (!isNaN(userNum)) {
       const value = result + userNum
       value > 0
@@ -34,14 +31,13 @@ class Calculator extends Component {
     }
   }
 
-
   render() {
     const {result} = this.state
     return (
         <div className='calc-wrapper'>
           <Title/>
           <Result result={result}/>
-          <BtnWrapper plus={this.plus} minus={this.minus} reset={this.reset}/>
+          <BtnWrapper plus_minus={this.plus_minus} reset={this.reset} arrNums={this.arrNums}/>
           <InputWrapper addYourNum={this.addYourNum}/>
         </div>
     );
--- src/components/calculator/btnWrapper/BtnWrapper.js
+++ src/components/calculator/btnWrapper/BtnWrapper.js
@@ -3,15 +3,11 @@ import './BtnWrapper.css'
 
 class BtnWrapper extends Component {
   render() {
-    const {plus, minus, reset} = this.props
+    const {plus_minus, reset, arrNums} = this.props
+
     return (
         <div className='btn-wrapper'>
-          <button onClick={() => plus(1)} className='btn'>+ 1</button>
-          <button onClick={() => minus(1)} className='btn'>- 1</button>
-          <br/>
-          <button onClick={() => plus(100)} className='btn'>+ 100</button>
-          <button onClick={() => minus(100)} className='btn'>- 100</button>
-          <br/>
+          {arrNums.map(value => <button onClick={() => plus_minus(value)} className='btn'>{value}</button>)}
           <button onClick={() => reset()} className='btn'>Reset</button>
         </div>
     );
